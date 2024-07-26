7f833d43bae04a469830c1b7aed5c2284c723cac
YuXiangLo <yuxiang@yuxiang.local>
2024-07-26 13:28:26 +0800

    update

--- 1636.cpp
+++ 1636.cpp
@@ -4,7 +4,6 @@ public:
         int freq[201] = {};
         for (int i : nums)
             freq[i+100]++;
-       
         sort(nums.begin(), nums.end(), [&](int a, int b){return freq[a+100] < freq[b+100] || freq[a+100] == freq[b+100] && a > b;});
         return nums;
     }
